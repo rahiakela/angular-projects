5c734d79ea4148b6cd7057b3569da91f861802c3
tecknocracy <editors@tecknocracy.com>
2018-07-01 13:26:29 +0530

    layout is done

---
+++ angular-social/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LayoutComponent} from './ui/components/layout/layout.component';
+
+const routes: Routes = [
+  { path: '', component: LayoutComponent, children: []}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
