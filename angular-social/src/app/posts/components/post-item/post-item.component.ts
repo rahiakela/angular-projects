03c70f7a7457c0954c68d37bb6e190a86c579469
rahiakela <rahi.akela@yahoo.com>
2018-07-10 09:26:17 +0530

    post detail and profile detail is done

---
+++ angular-social/src/app/posts/components/post-item/post-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css'],
+})
+export class PostItemComponent implements OnInit {
+  @Input() post: any
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ angular-social/src/app/posts/components/profile-item/profile-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-profile-item',
+  templateUrl: './profile-item.component.html',
+  styleUrls: ['./profile-item.component.css'],
+})
+export class ProfileItemComponent implements OnInit {
+  @Input() profile: any
+  constructor() {}
+
+  ngOnInit() {}
+}
